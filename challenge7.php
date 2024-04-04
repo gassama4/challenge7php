476bdfab35028e5ea2fbe04b324afa5d395a93cb
gassama4 <161436194+gassama4@users.noreply.github.com>
2024-04-04 17:44:04 +0200

    Add files via upload

---
+++ challenge7.php
@@ -0,0 +1,15 @@
+<?php
+
+$presentation = [
+    'nom_complet' => [
+        'prenom' => 'SIDYA',
+        'nom' => 'GASSAMA',
+    ],
+    'ville' => 'Les Pavillons-sous-Bois',
+    'arme_zombie' => 'lampe torche',
+];
+
+
+var_dump($presentation); 
+
+?>
